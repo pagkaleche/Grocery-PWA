21df485aa034c8a014644ed7602f682e3a37e784
Adrian Landia <146389578+pagkaleche@users.noreply.github.com>
2025-01-20 18:23:42 -0500

    update3

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
-import { db } from "./firebaseconfig";
+
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDo9nRtzMTFaGFfGWgqlcksi5Y9h7x46x0",
+    authDomain: "webdevtrends-59dcf.firebaseapp.com",
+    projectId: "webdevtrends-59dcf",
+    storageBucket: "webdevtrends-59dcf.firebasestorage.app",
+    messagingSenderId: "711058905449",
+    appId: "1:711058905449:web:b29e4821f76656e45cc5f8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
 
 // Add Task
 // addTaskBtn.addEventListener('click', () => {
