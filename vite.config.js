127a94cf522c3e537c57ca5f9141e92a552d6727
Adrian Landia <146389578+pagkaleche@users.noreply.github.com>
2025-01-29 10:47:04 -0500

    base set for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    outDir: 'docs',
-  }
+    base: process.env.NODE_ENV === 'production' ? '/WebDevTrends/' : '/',
+    build: {
+        outDir: 'docs',
+    }
 });
