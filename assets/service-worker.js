887d3d1fbaf752d11c928f7744cc72fef3512499
Adrian Landia <146389578+pagkaleche@users.noreply.github.com>
2025-01-29 11:32:17 -0500

    Updates

---
+++ assets/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'to-do-pwa-cache-v1';
+const FILES_TO_CACHE = [
+    '/WebDevTrends/',
+    '/WebDevTrends/index.html',
+    '/WebDevTrends/style.css',
+    '/WebDevTrends/app.js',
+    '/WebDevTrends/manifest.json',
+    '/WebDevTrends/icons/icon-128.png',
+    '/WebDevTrends/icons/icon-512.png'
+
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
